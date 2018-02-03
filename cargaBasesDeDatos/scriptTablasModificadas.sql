f8123cf3e904acfbd2c0ed4034f66c81cacb8671
Javier Ramos Fernández <alu0100884982@ull.edu.es>
2018-02-03 00:09:43 +0000

    Script de carga de la base de datos de las tablas modificadas cambiado

--- cargaBasesDeDatos/scriptTablasModificadas.sql
+++ cargaBasesDeDatos/scriptTablasModificadas.sql
@@ -31,7 +31,7 @@ travel_seq varchar(402),
 travel_time float);
 
 COPY vehicle_trajectories_training_modified from '/home/javisunami/Escritorio/TFG/datasetsOriginales/training/trajectories_table 5_training.csv' WITH CSV HEADER;
-UPDATE vehicle_trajectories_training_modified SET travel_seq = STRING_TO_ARRAY(travel_seq, '#');
+UPDATE vehicle_trajectories_training_modified SET travel_seq = STRING_TO_ARRAY(travel_seq, ';');
 
 ALTER TABLE vehicle_trajectories_training_modified ALTER travel_seq type varchar(100) ARRAY using travel_seq::varchar(100) ARRAY;
 
@@ -50,24 +50,30 @@ DECLARE
    link link_object;
    conjunto_links link_object[] DEFAULT '{}';
    campo varchar(100);
+   array_componentes_campo varchar(40) ARRAY;
+   contador int := 0;
 BEGIN
    OPEN curs1;
    LOOP
+        contador := contador + 1;
         FETCH curs1 INTO t_row;
+        EXIT WHEN t_row IS NULL;
         FOREACH campo IN ARRAY t_row.travel_seq
         LOOP
-              RAISE NOTICE '%s', campo;/**
-              link.id := campo[0];
-              link.entrance_time := campo[1];
-              link.duration := campo[2];
-              conjunto_links := conjunto_links || link;**/
-        END LOOP;/**
-        UPDATE vehicle_trajectories_training_modified SET t_row.travel_seq = conjunto_links;**/
+              array_componentes_campo := STRING_TO_ARRAY(campo, '#');
+              link.id :=  array_componentes_campo[1];
+              link.entrance_time :=  array_componentes_campo[2];
+              link.duration :=  array_componentes_campo[3];
+              conjunto_links := conjunto_links || link;
+        END LOOP;
+        UPDATE vehicle_trajectories_training_modified SET travel_seq = conjunto_links
+                WHERE CURRENT OF curs1;
         conjunto_links := '{}';
-        EXIT WHEN NOT FOUND;
+        link = null;
    END LOOP;
    CLOSE curs1;
 END first_block $$;
 
+ALTER TABLE vehicle_trajectories_training_modified ALTER travel_seq type link_object[] using travel_seq::link_object[];
 
 

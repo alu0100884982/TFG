895ef8d5f5c6ac9bd860ed0349edd9e2017e9c17
Javier Ramos Fernández <alu0100884982@ull.edu.es>
2018-02-05 19:00:14 +0000

    Fichero de gráficas modificado

--- graficas/travel_time_per_window_graphic.py
+++ graficas/travel_time_per_window_graphic.py
@@ -12,17 +12,17 @@ cur.execute("""SELECT *
 FROM travel_time_intersection_to_tollgate_modified ;""")
 rows = cur.fetchall()
 dates = sorted(set([date[2][0].date().strftime('%Y-%m-%d') for date in rows]))
-intersections = sorted(set([item[0] for item in rows]))
-tollgates = set([item[1] for item in rows])
-print(dates)
-horas = []
-valores = []
-for row in rows:
-       if (row[0] == 'C' and row[1] == 1 and row[2][0].date().strftime('%Y-%m-%d') == "2016-07-19") :
-               horas.append(row[2][0]);
-               valores.append(row[3]); 
-plt.plot(horas,valores)
-plt.title('Julio de 2016')
-plt.ylabel('Tiempo medio de viaje')
-plt.xlabel('Momento del día')
-plt.show()
+pairs=[['B',1],['B',3],['A',2],['A',3],['C',1],['C',3]]
+keys = {}
+
+
+for date in dates:
+        for pair in pairs:
+                keys[(pair[0],pair[1],date)] = []
+
+print(keys);
+#plt.plot(horas,valores)
+#plt.title('Julio de 2016')
+#plt.ylabel('Tiempo medio de viaje')
+#plt.xlabel('Momento del día')
+#plt.show()

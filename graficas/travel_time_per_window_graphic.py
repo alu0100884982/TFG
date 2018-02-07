ddea3110532483756e0cca97bd2cf5d505cb3a72
Javier Ramos Fernández <alu0100884982@ull.edu.es>
2018-02-07 03:10:13 +0000

    Modificado fichero de gráficas para mostrar por horas de todos los días recogidos el tráfico en cada una de las intersecciones

--- graficas/travel_time_per_window_graphic.py
+++ graficas/travel_time_per_window_graphic.py
@@ -1,6 +1,8 @@
 
 import psycopg2
-import matplotlib.pyplot as plt;
+import matplotlib.pyplot as plt
+import collections as coll
+import matplotlib.dates as dates
 
 try:
     conn = psycopg2.connect("dbname='tfgdatosmodificados' user='javisunami' host='localhost' password='javier123'")
@@ -11,8 +13,9 @@ cur = conn.cursor()
 cur.execute("""SELECT *
 FROM travel_time_intersection_to_tollgate_modified ;""")
 rows = cur.fetchall()
-dates = sorted(set([date[2][0].date().strftime('%Y-%m-%d') for date in rows]))
+dates = sorted(set([date[2][1].date().strftime('%Y-%m-%d') for date in rows]))
 pairs=[['B',1],['B',3],['A',2],['A',3],['C',1],['C',3]]
+pairs = sorted(pairs);
 keys = {}
 
 
@@ -20,9 +23,24 @@ for date in dates:
         for pair in pairs:
                 keys[(pair[0],pair[1],date)] = []
 
-print(keys);
-#plt.plot(horas,valores)
+counter = 0;
+for row in rows:
+        keys[(row[0], row[1], row[2][1].date().strftime('%Y-%m-%d') )].append([row[2][1].time(), row[3]])
+        
+keys = coll.OrderedDict(sorted(keys.items()))
+
+for element in pairs:
+   horas = []
+   valores = []
+   for key, value in keys.items() :
+        
+        if (key[0] == element[0] and key[1] == element[1]):
+            horas = [pair[0].strftime("%H.%M") for pair in value]
+            valores = [pair[1] for pair in value];
+            print([float(hora) for hora in horas])
+            plt.plot(horas, valores); 
+   print("Intersección: ", element[0], " Tollgate : ", element[1]);
+   plt.show();  
 #plt.title('Julio de 2016')
 #plt.ylabel('Tiempo medio de viaje')
 #plt.xlabel('Momento del día')
-#plt.show()

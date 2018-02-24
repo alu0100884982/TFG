3e6c303e3c32e6b09dea5788b5df1e4fc0890c5b
Javier Ramos Fernández <alu0100884982@ull.edu.es>
2018-02-24 21:26:29 +0000

    Modificaciones realizadas de la primera aproximación de predicciones

--- machineLearningProyecto/Aproximaciones/Primera aproximacion/primera_aproximacion_tfgtest1.sql
+++ machineLearningProyecto/Aproximaciones/Primera aproximacion/primera_aproximacion_tfgtest1.sql
@@ -1,42 +1,77 @@
 CREATE TYPE tipo_fila AS (intersection char(1),tollgate int, left_side_interval timestamp );
-CREATE OR REPLACE FUNCTION create_firstrow_route_interval(ruta-intervalo tipo_fila) 
+CREATE OR REPLACE FUNCTION create_firstrow_route_interval(rutaintervalo tipo_fila) 
     RETURNS void AS $$
     DECLARE
     BEGIN
-           UPDATE tabla_resultado_average_travel_time 
+           UPDATE tabla_resultado_average_travel_time AS thistable
                      SET twenty_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '20 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval)
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
-           UPDATE tabla_resultado_average_travel_time 
-                    SET forty_min_previous = AVG(othertable.avg_travel_time)
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '20 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval)
+                    AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate AND
+                    thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo.intersection AND  thistable.tollgate_id = rutaintervalo.tollgate;
+         
+           UPDATE tabla_resultado_average_travel_time AS thistable
+                    SET forty_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '40 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval - '20 minute')
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
-           UPDATE tabla_resultado_average_travel_time 
-                    SET sixty_min_previous = AVG(othertable.avg_travel_time)
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '40 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval - INTERVAL '20 minute') AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate  AND
+                     thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND thistable.intersection_id = rutaintervalo.intersection AND  thistable.tollgate_id = rutaintervalo.tollgate;
+          
+           UPDATE tabla_resultado_average_travel_time AS thistable
+                    SET sixty_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '60 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval - '40 minute')
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
-           UPDATE tabla_resultado_average_travel_time 
-                    SET eighty_min_previous = AVG(othertable.avg_travel_time)
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '60 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval - INTERVAL '40 minute') AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate  AND
+                     thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo.intersection AND thistable.tollgate_id = rutaintervalo.tollgate;
+                    
+           UPDATE tabla_resultado_average_travel_time AS thistable
+                    SET eighty_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '80 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval)
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
-           UPDATE tabla_resultado_average_travel_time 
-                    SET onehundred_min_previous = AVG(othertable.avg_travel_time)
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '80 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval - INTERVAL '60 minute')  AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate  AND
+                     thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo.intersection AND  thistable.tollgate_id = rutaintervalo.tollgate;
+                    
+           UPDATE tabla_resultado_average_travel_time AS thistable
+                    SET onehundred_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '100 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval)
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
-           UPDATE tabla_resultado_average_travel_time 
-                    SET onehundredtwenty_min_previous = AVG(othertable.avg_travel_time)
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '100 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval - INTERVAL '80 minute') AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate AND
+                     thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo.intersection AND  thistable.tollgate_id = rutaintervalo.tollgate;
+                    
+           UPDATE tabla_resultado_average_travel_time AS thistable
+                    SET onehundredtwenty_min_previous = othertable.avg_travel_time
                     FROM travel_time_intersection_to_tollgate_test1 othertable
-                    WHERE othertable.time_window[1] = (ruta-intervalo.left_side_interval - '120 minute') AND othertable.time_window[2] = (ruta-intervalo.left_side_interval)
-                    AND othertable.intersection_id = ruta_intervalo.intersection AND othertable.tollgate_id = ruta_intervalo.tollgate;
- = 
+                    WHERE othertable.time_window[1] = (rutaintervalo.left_side_interval - INTERVAL '120 minute') AND othertable.time_window[2] = (rutaintervalo.left_side_interval - INTERVAL '100 minute')  AND othertable.intersection_id = rutaintervalo.intersection AND othertable.tollgate_id = rutaintervalo.tollgate AND
+                     thistable.time_window[1] = rutaintervalo.left_side_interval AND  thistable.time_window[2] = (rutaintervalo.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo.intersection AND  thistable.tollgate_id = rutaintervalo.tollgate;
     END;
     $$ LANGUAGE plpgsql;
     
+CREATE OR REPLACE FUNCTION actualizar_filaactual_con_filaanterior(rutaintervalo_actual tipo_fila, rutaintervalo_anterior tipo_fila) 
+    RETURNS void AS $$
+    DECLARE
+    BEGIN
+   
+    
+    UPDATE tabla_resultado_average_travel_time AS thistable
+                     SET twenty_min_previous = othertable.avg_travel_time
+                    FROM travel_time_intersection_to_tollgate_test1 othertable
+                    WHERE othertable.time_window[1] = (rutaintervalo_actual.left_side_interval - INTERVAL '20 minute') AND othertable.time_window[2] = (rutaintervalo_actual.left_side_interval)
+                    AND othertable.intersection_id = rutaintervalo_actual.intersection AND othertable.tollgate_id = rutaintervalo_actual.tollgate AND
+                     thistable.time_window[1] = rutaintervalo_actual.left_side_interval AND  thistable.time_window[2] = (rutaintervalo_actual.left_side_interval + INTERVAL '20 minute')
+                    AND  thistable.intersection_id = rutaintervalo_actual.intersection AND  thistable.tollgate_id = rutaintervalo_actual.tollgate;
+                    
+      UPDATE tabla_resultado_average_travel_time AS actual
+                     SET forty_min_previous = before.twenty_min_previous,
+                         sixty_min_previous = before.forty_min_previous,
+                         eighty_min_previous = before.sixty_min_previous,
+                         onehundred_min_previous = before.eighty_min_previous,
+                         onehundredtwenty_min_previous = before.onehundred_min_previous
+                     FROM tabla_resultado_average_travel_time before
+                     WHERE actual.intersection_id = rutaintervalo_actual.intersection AND actual.tollgate_id = rutaintervalo_actual.tollgate AND actual.time_window[1]=                      rutaintervalo_actual.left_side_interval AND before.intersection_id = rutaintervalo_anterior.intersection AND before.tollgate_id = rutaintervalo_anterior.tollgate AND before.time_window[1] =      rutaintervalo_anterior.left_side_interval;
+    END;
+    $$ LANGUAGE plpgsql;
 /**
 CREATE OR REPLACE FUNCTION create_columns_intervals()    
 RETURNS void AS $$
@@ -66,7 +101,7 @@ DO $$
 DECLARE
   termina boolean DEFAULT FALSE;
   rutas_intervalos tipo_fila ARRAY;
-  ruta_intervalo_anterior tipo_fila;
+  rutaintervalo_anterior tipo_fila;
   contador integer DEFAULT 1;
 BEGIN 
   rutas_intervalos := ARRAY(SELECT '(' ||intersection_id || ', ' || tollgate_id || ', ' || time_window[1] || ')' FROM tabla_resultado_average_travel_time ORDER BY intersection_id, tollgate_id, time_window);
@@ -74,14 +109,15 @@ BEGIN
   WHILE contador <= ARRAY_LENGTH(rutas_intervalos, 1) LOOP
        termina := FALSE;
        PERFORM create_firstrow_route_interval(rutas_intervalos[contador]);
-       ruta_intervalo_anterior = rutas_intervalos[contador];
+       rutaintervalo_anterior = rutas_intervalos[contador];
        contador = contador + 1;
         WHILE NOT(termina) LOOP
-            IF ((rutas_intervalos[contador] - ruta_intervalo_anterior) = INTERVAL '20 min') THEN
-                actualizar_filaactual_con_filaanterior(rutas_intervalos[contador], ruta_intervalo_anterior)
+            IF (rutas_intervalos[contador].intersection = rutaintervalo_anterior.intersection AND rutas_intervalos[contador].tollgate = rutaintervalo_anterior.tollgate AND  (rutas_intervalos[contador].left_side_interval - rutaintervalo_anterior.left_side_interval) = INTERVAL '20 min') THEN 
+                PERFORM actualizar_filaactual_con_filaanterior(rutas_intervalos[contador], rutaintervalo_anterior);
+                 rutaintervalo_anterior = rutas_intervalos[contador];
                 contador := contador + 1;
             ELSE 
-                termina := TRUE
+                termina := TRUE;
             END IF;
         END LOOP;
   END LOOP;
@@ -89,7 +125,7 @@ BEGIN
 END block $$;
 
 /**
-   EXECUTE('CREATE TABLE ' || ruta-intervalo || '(intersection_id char(1) CONSTRAINT has_intersection_id_value CHECK (intersection_id IN (\'A\', \'B\', \'C\')),
+   EXECUTE('CREATE TABLE ' || rutaintervalo || '(intersection_id char(1) CONSTRAINT has_intersection_id_value CHECK (intersection_id IN (\'A\', \'B\', \'C\')),
         tollgate_id smallint CONSTRAINT has_tollgate_id_value CHECK (tollgate_id  IN (1,2,3)),
         time_window timestamp ARRAY[2],
         twenty_min_previous float,
